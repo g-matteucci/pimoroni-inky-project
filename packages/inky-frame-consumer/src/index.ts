cef08e6b6c1b7ee8179073fe888b0544e51fbd83
Carmine Porricelli <carmineporricelli96@gmail.com>
2025-07-25 20:37:42 +0200

    Update index.ts

--- packages/inky-frame-consumer/src/index.ts
+++ packages/inky-frame-consumer/src/index.ts
@@ -1,5 +1,7 @@
 import { consumeInkyMatteucciEvents, LoggerService } from "inky-matteucci-commons";
 import { match } from "ts-pattern";
+import { spawnSync } from "child_process";
+import os from "os";
 
 const logger = new LoggerService("inky-frame-consumer");
 
@@ -9,6 +11,9 @@ consumeInkyMatteucciEvents((event) => {
   match(event)
     .with({ type: "display_photo" }, (e) => {
       logger.info("Displaying photo:", e.data.photoUrl);
+      spawnSync("python", [`${os.homedir()}/Pimoroni/inky/examples/spectra6/image.py`, "--file", e.data.photoUrl], {
+        stdio: "inherit",
+      });
     })
     // Do nothing
     .with({ type: "added_photo" }, (e) => {})
